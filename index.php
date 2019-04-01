6a16e641e6413dcf5a2aedc5315c1b89e9b20009
Marijus84 <marijus.romanovas@hotmail.com>
2019-04-01 19:46:02 +0300

    pataisyti trūkumai

--- index.php
+++ index.php
@@ -8,18 +8,18 @@ require 'vendor/autoload.php';
 
 $output = \calculateHomeWorkSum(3, 2.2, '1');
 
-echo "calculateHomeWorkSum: $output <br>" ;
+echo "Funkcijos calculateHomeWorkSum(...\$numbers); iš root namespace rezultatas: <br> $output <br>" ;
 
 $output = Nfq\Akademija\Not_Typed\calculateHomeWorkSum(3, 2.2, '1');
 
-echo "calculateHomeWorkSum: $output <br>" ;
+echo "Funkcijos calculateHomeWorkSum(...\$numbers): int; iš Nfq\Akademija\Not_Typed namespace rezultatas: <br> $output <br>" ;
 
 $output = Nfq\Akademija\Soft\calculateHomeWorkSum(3, 2.2, '1');
 
-echo "calculateHomeWorkSum: $output <br>" ;
+echo "Funkcijos calculateHomeWorkSum(int...\$numbers): int; iš Nfq\Akademija\Soft namespace rezultatas: <br>  $output <br>" ;
 
 $output = Nfq\Akademija\Strict\calculateHomeWorkSum(3, 2.2, '1');
 
-echo "calculateHomeWorkSum: $output" ;
+echo "Funkcijos calculateHomeWorkSum(int...\$numbers): int; iš Nfq\Akademija\Strict namespace su declare(strict_types=1) direktyva rezultatas: <br>  $output <br>" ;
 
 ?>
